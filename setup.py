8e7535c54016223762bc4852ef32e67cf9c454e8
senateurritchie <zakeszako@yahoo.fr>
2023-10-06 17:02:13 +0000

    add ci/cd pipeline on pypi v7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='messenger_bus',
-    version='1.0.9-alpha',
+    version='1.0.9',
     description='Bus messaging system',
     url='https://github.com/CoteOuestAudiovisuel/messenger_bus',
     author='Zacharie Assagou',
